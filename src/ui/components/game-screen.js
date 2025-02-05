80c483f3299a473659f1fef3e062dfa677a78efa
Nathaniel <ramirezNa22@students.ecu.edu>
2025-02-05 13:01:36 -0500

    Starting creation on the game-screen.js file
    Currently only added 1 import and comments outlining other imports that might be needed but don't have the associated files created yet
    JiraID:DND-4
    
    Written By: @NathanielR2
    ToDo: Add the basic outline for the needed functions concerning utilizing the game logic along with displaying the page

---
+++ src/ui/components/game-screen.js
@@ -0,0 +1,5 @@
+import {Component, html} from '../lib.js'
+
+//Need an import for the new game script
+//Need to import cards when they are done
+//Need to import functions to test the current game condition such as the current room and its status along with whether the dungeon is complete or not
