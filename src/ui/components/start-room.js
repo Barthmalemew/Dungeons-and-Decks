1abfec685053cdee19ea4f32313438c7ccf130ab
Jesus Jimenez <jesusjimenez.mail@gmail.com>
2025-02-05 11:40:38 -0500

    v2.2.3 Created start room js page, has placeholder text and classes. CSS is needed at the moment

---
+++ src/ui/components/start-room.js
@@ -0,0 +1,22 @@
+import {html, Component} from '../lib.js'
+
+export default class StartRoom extends Component {
+	render() {
+		return html`
+        <div class="container">
+            <h1 center> Start </h1>
+            <p center>small description </p>
+
+            <div class="box"> 
+                <ul class="options">
+                    <li><button onClick=${() => this.props.onContinue()}>Open the map</button></li>
+                </ul>
+            </div
+
+            <p center>
+					<a href="/">Let me out</a>
+				</p>
+        </div>
+        `
+    }
+}
