cba58c252cdcf29938d40e84dc522c8de43b5542
huffti19 <huffti19@students.ecu.edu>
2025-03-05 09:58:03 -0500

    Add files via upload

---
+++ src/content/cards/Strike.js
@@ -0,0 +1,22 @@
+export default
+{
+    name: 'Strike',
+    type: 'attack',
+    energy: 1,
+    target: 'enemy',
+    damage: 7,
+    description: 'Strike a target with your blade and Deal 7 damage.',
+    image: 'Strike.png',
+}
+
+export const upgrade = (card) => {
+    return{
+        ...card,
+        powers: {
+            damage: 10,
+    },
+        description: 'Strike a target with your blade and Deal 10 damage.',
+    }
+
+}
+
